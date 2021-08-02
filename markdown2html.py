23d84b7a9ea6e2f2d1f7a4367890022baddf557e
Xvaria <Xvongola23@gmail.com>
2021-08-02 01:01:24 -0500

    Ready | Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 '''Start a script'''
-from sys import argv
+from sys import argv, stderr
 
 
 if __name__ == "__main__":
     if (len(argv) < 2):
-        print("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
     else:
         try:
             with open(argv[1], encoding="utf-8") as markdown:
                 exit
         except:
-            print("Missing {}".format(argv[1]))
+            print("Missing {}".format(argv[1]), file=stderr)
             exit(1)
